70253995436dbe762f3a5751daa3b9890b600a40
HEEWOONG PARK <pheewoong@gmail.com>
2020-07-26 22:59:45 +0900

    bonday

---
+++ js/m_main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  var swiper = new Swiper('.swiper-container', {
+    loop: true,
+    pagination: {
+    el: '.swiper-pagination',
+    type: 'fraction',
+    },
+    navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+    }
+  });
+})
